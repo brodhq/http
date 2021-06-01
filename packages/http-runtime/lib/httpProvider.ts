2828bda63913dcdb59966476cfb10ec45a457dae
mads hargreave <mads.hargreave@gmail.com>
2021-06-01 13:12:25 +0200

    released

--- packages/http-runtime/lib/httpProvider.ts
+++ packages/http-runtime/lib/httpProvider.ts
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch'
 import { HttpConfig } from './httpConfig'
 import { NodeHttp } from './httpFacade'
+import { Http } from './httpTypes'
 
 export function config({
     fetchFn = fetch,
     ...config
-}: Partial<HttpConfig> & Pick<HttpConfig, 'events'>) {
+}: Partial<HttpConfig> & Pick<HttpConfig, 'events'>): Http {
     return new NodeHttp({ fetchFn, ...config })
 }
